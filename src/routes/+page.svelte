ad283dba9c14357f00186aacd0b6066747002250
null <null>
2024-10-29 11:10:22 -0700

    Replicated issue

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script>
-  import { Heading, P, List, Li, Label, MultiSelect } from 'flowbite-svelte';
+  import { Heading, P, List, Li, Label, MultiSelect, Button, Datepicker, Dropdown, DropdownItem } from 'flowbite-svelte';
+	import { ChevronDownOutline } from 'flowbite-svelte-icons';
+
+  
+	const rowClass = 'border-b-2 border-gray-300 flex items-center p-4 whitespace-nowrap';
+	const cellClass = 'inline-flex basis-10/12';
+	const headerCellClass = `inline-flex basis-2/12 font-bold w-28 mr-2`;
+	const buttonClass = 'mx-2';
+	const today = new Date();
+	let dateRange = { from: today, to: today };
 </script>
 
 <div class='p-9'>
@@ -13,3 +22,28 @@
 
 </div>
 
+<div class={rowClass}>
+	<div class={headerCellClass}>Date:</div>
+	<div class={cellClass}>
+		<Button
+			class="bg-transparent py-0 text-center text-sm font-medium text-gray-500 hover:bg-transparent hover:text-gray-900 focus:ring-transparent dark:bg-transparent dark:text-gray-400 dark:hover:bg-transparent dark:hover:text-white dark:focus:ring-transparent"
+			>Today<ChevronDownOutline class="m-2.5 ms-1.5 w-2.5" /></Button
+		>
+		<Dropdown class="w-40">
+			<DropdownItem>Yesterday</DropdownItem>
+			<DropdownItem>Today</DropdownItem>
+			<DropdownItem>Last 7 days</DropdownItem>
+			<DropdownItem>Last 30 days</DropdownItem>
+			<DropdownItem>Last 90 days</DropdownItem>
+		</Dropdown>
+		<div class="basis-10/12">
+			<Datepicker
+				color="purple"
+				range
+				required={true}
+				bind:rangeFrom={dateRange.from}
+				bind:rangeTo={dateRange.to}
+			/>
+		</div>
+	</div>
+</div>
